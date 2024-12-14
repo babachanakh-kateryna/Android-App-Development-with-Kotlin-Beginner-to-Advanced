ec679b3d41c9838a571eb851faa1dd171fb26d32
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-14 22:51:27 +0100

    16. Data Types in Android Kotlin

---
+++ MyApplication/app/src/main/java/com/example/myapplication/DataTypes.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+fun main (args:Array<String>){
+    var a:Boolean = true
+    var b:Char = 'A'
+    var c:Byte = 10
+    var d:Short = -356
+    var e:Int = 1000
+    var f:Long = -4593939393L
+    var g:Float = 11.6f
+    var h:Double = 11.6
+
+    println("Boolean: $a")
+    println("Char: $b")
+    println("Byte: $c")
+    println("Short: $d")
+    println("Int: $e")
+    println("Long: $f")
+    println("Float: $g")
+    println("Double: $h")
+}
