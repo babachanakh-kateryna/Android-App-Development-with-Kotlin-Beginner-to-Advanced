c0c25dece079fb488c176acdbe74a0b22d993c87
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-15 00:21:00 +0100

    26. Equality and Relational Operators in ktln

---
+++ MyApplication/app/src/main/java/com/example/myapplication/section4/EqualityOperators.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication.section4
+
+fun main(args: Array<String>) {
+
+    var a:Int = 10
+    var b:Int = 20
+
+    println("a == b : " + (a == b))
+    println("a != b : " + (a != b))
+    println("a <= b : " + (a <= b))
+    println("a < b : " + (a < b))
+    println("a >= b : " + (a >= b))
+    println("a > b : " + (a > b))
+
+
+}
