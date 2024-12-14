3e96d5f465b229c7682cd00d330df39147464b44
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-14 23:30:43 +0100

    20. ArrayList in Kotlin Android

---
+++ MyApplication/app/src/main/java/com/example/myapplication/ArrayList.kt
@@ -0,0 +1,35 @@
+package com.example.myapplication
+
+fun main(args: Array<String>) {
+
+    var age = ArrayList<Int>()
+
+    age.add(1)
+    age.add(1, 2)
+    age.add(220)
+
+    println("First Element: ${age[0]}")
+    println("Second Element: ${age.get(1)}")
+    println("Third Element: ${age[2]}")
+
+    println("-------------------")
+
+    age.remove(1)
+    println("First Element: ${age[0]}")
+    println("Second Element: ${age.get(1)}")
+    println(age.size)
+
+    println("-------------------")
+    var cars = arrayListOf<String>("BMW", "Audi")
+    cars.add("Toyota")
+    println("First Car: ${cars[0]}")
+    println("Second Car: ${cars.get(1)}")
+    println("Third Car: ${cars[2]}")
+
+    println("-------------------")
+    var mixArray = arrayListOf<Any>("BMW", 25, "Audi", false)
+    println("First Car: ${mixArray[0]}")
+    println("First Car Age: ${mixArray[1]}")
+    println("Second Car: ${mixArray.get(2)}")
+    println("Second Car Age: ${mixArray[3]}")
+}
