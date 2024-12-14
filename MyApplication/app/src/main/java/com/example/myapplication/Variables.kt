be69a75b9f931df72dbc2f41c8c2d9bcb3d49339
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-14 22:44:31 +0100

    15. Variables in Kotlin

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Variables.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+fun main (args:Array<String>){
+    var a = 10
+    var b = 20
+    var c = a + b
+
+    println("The sum of $a and $b is $c")
+
+    var age = 25
+    age = 30
+    println("Age is $age")
+
+    val year = 2021
+    // year = 2022 // This will give an error
+}
