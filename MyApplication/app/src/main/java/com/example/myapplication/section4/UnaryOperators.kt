84c8d1f2434ba1c7e5b2bfe9964abc81d4d15399
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-15 00:16:16 +0100

    25. Unary Operators in Kotlin Android

---
+++ MyApplication/app/src/main/java/com/example/myapplication/section4/UnaryOperators.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication.section4
+
+fun main(args: Array<String>) {
+
+    var num:Double = 25.5
+    var isCheck:Boolean = true
+
+    println("+num = ${+num}")
+    println("-num = ${-num}")
+    println("++num = ${++num}")
+    println("--num = ${--num}")
+    println("!isCheck = ${!isCheck}")
+
+
+    println("-----------------")
+    var res:Double = 4.7
+    println("res = $res")
+    println("res++ = ${res++}")
+    println("res = $res")
+    println("++res = ${++res}")
+    println("res = $res")
+
+}
