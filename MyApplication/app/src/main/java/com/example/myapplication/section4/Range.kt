25d24e782b83afb8440beb909a588e6f63276ff4
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-15 10:34:51 +0100

    29. The “rangeTo()” Function and “in” operator

---
+++ MyApplication/app/src/main/java/com/example/myapplication/section4/Range.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.section4
+
+fun main(args: Array<String>) {
+
+
+    var myCharRange = 'a'.rangeTo('j')
+    var myChar = 'k' in myCharRange
+
+    println(myCharRange)
+    println(myChar)
+}
