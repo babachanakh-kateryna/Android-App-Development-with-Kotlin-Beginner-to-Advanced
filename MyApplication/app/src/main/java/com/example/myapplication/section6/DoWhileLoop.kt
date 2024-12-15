21976f692aa3e3a948f44a28463964e76a27d8d1
Babachanakh Kateryna <katrin.ltvnv@gmail.com>
2024-12-15 12:02:56 +0100

    36. Kotlin: Do-While Loop

---
+++ MyApplication/app/src/main/java/com/example/myapplication/section6/DoWhileLoop.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication.section6
+
+fun main(args: Array<String>) {
+    var num = 1
+    do {
+        println("Hello World")
+        println(num)
+        num++
+        println(num)
+    } while (num <= 5)
+}
